4121e27a0a30cbefe563f55691b063a2904e6a4f
Chase Gaddis <cgaddis36@gmail.com>
2022-12-14 14:06:54 -0600

    add status enum

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,13 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :order_items 
   has_many :items, :through => :order_items
+  enum status: {
+    placed: 0,
+    shipped: 1,
+    delivered: 2,
+    inquiry: 3, 
+    refunded: 4,
+    returned_and_refunded: 5,
+    canceled: 6
+  }
 end
