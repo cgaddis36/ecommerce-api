73614eea2b8a09436090a147e08868ec3601eee0
Chase Gaddis <cgaddis36@gmail.com>
2022-12-19 19:18:07 -0600

    add query for finding items by subcategory

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -6,5 +6,6 @@ module Types
     field :all_items, resolver: Queries::Items::AllItems
     field :find_item, resolver: Queries::Items::FindItem
     field :find_items, resolver: Queries::Items::FindItems
+    field :find_items_by_subcategory, resolver: Queries::Items::FindItemsBySubcategory
   end
 end
