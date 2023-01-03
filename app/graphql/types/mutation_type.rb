8bc30d2ee4eaeef691c6966dcc777cb6b9c82aad
Chase Gaddis <cgaddis36@gmail.com>
2023-01-03 14:46:50 -0600

    add field for create_order

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,7 @@ module Types
   class MutationType < Types::BaseObject
     field :add_cart_items, mutation: Mutations::Users::AddCartItems
     field :delete_cart_items, mutation: Mutations::Users::DeleteCartItems
+    
+    field :create_order, mutation: Mutations::Orders::CreateOrder
   end
 end
